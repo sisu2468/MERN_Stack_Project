a28062be0873abc9a9e8cede0176ba810ab4ed75
Bhav Beri <bhavberi@gmail.com>
2023-03-10 00:36:08 +0530

    Added DDOS Protection

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,24 +2,34 @@ from fastapi import FastAPI
 from os import getenv
 from routers import users
 
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
+
 DEBUG = getenv("BACKEND_DEBUG", "False").lower() in ("true", "1", "t")
 
 # Create a new FastAPI instance
 # FastAPI App
-if DEBUG :
+if DEBUG:
     app = FastAPI(
         debug=DEBUG,
         title="ChargeMate",
-        desciption="DASS Project 2k23 - Team-37",
+        description="DASS Project 2k23 - Team-37",
     )
 else:
     app = FastAPI(
         debug=DEBUG,
         title="ChargeMate",
-        desciption="DASS Project 2k23 - Team-37",
+        description="DASS Project 2k23 - Team-37",
         docs_url=None,
         redoc_url=None
     )
+
+limiter = Limiter(key_func=get_remote_address, default_limits=["50/minute"])
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
 
 # Mount the user router on the "/auth" path
 app.include_router(users.router, prefix="/auth", tags=["auth"])
