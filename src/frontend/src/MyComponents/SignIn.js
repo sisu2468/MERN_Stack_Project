b5111222aff3293cd600962082155b65d4a75eb3
tanalpha-aditya <raghuvanshiadityapiloy@gmail.com>
2023-04-13 01:35:41 +0530

    Login working

--- src/frontend/src/MyComponents/SignIn.js
+++ src/frontend/src/MyComponents/SignIn.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -12,6 +12,8 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
+
 
 function Copyright(props) {
   return (
@@ -25,34 +27,86 @@ function Copyright(props) {
     </Typography>
   );
 }
-
-
-
-
 const theme = createTheme();
 
+
+
+
 const SignIn = () => {
+  const [input, setInput] = useState({
+    username: "",
+    password: "",
+  })
+  const nav = useNavigate()
+
+
+
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   const data = new FormData(event.currentTarget);
+  //   // console.log({
+  //   //   username: data.get('Username'),
+  //   //   password: data.get('password'),
+  //   // });
+
+  //   console.log(input.username, input.password)
+  //   console.log("username, password")
+
+  //   const url = "http://localhost:80/api/admin/auth/login";  // The URL of your API endpoint
+  //   const username = "your_username";  // The username for sign-in
+  //   const password = "your_password";  // The password for sign-in
+
+  //   // Prepare the request data
+  //   data.append("username", input.username);
+  //   data.append("password", input.password);
+
+  //   // Make the POST request to the API
+  //   fetch(url, {
+  //     method: "POST",
+  //     body: data
+  //   })
+  //     .then(response => {
+  //       if (response.ok) {
+  //         // Extract the response data
+  //         return response.json();
+  //       } else {
+  //         throw new Error(`Error: ${response.status} - ${response.statusText}`);
+  //       }
+  //     })
+  //     .then(responseData => {
+  //       // Access the access token, token type, and role from the response data
+  //       const access_token = responseData.access_token;
+  //       const token_type = responseData.token_type;
+  //       const role = responseData.role;
+  //       // localStorage.setItem('token',response.data.data.token)
+  //       localStorage.setItem("loggedin", true)
+  //       // Do whatever you want with the access token, token type, and role
+  //       console.log(`Access Token: ${access_token}`);
+  //       console.log(`Token Type: ${token_type}`);
+  //       console.log(`Role: ${role}`);
+  //     })
+  //     .catch(error => {
+  //       // Handle the error response
+  //       console.error(error);
+  //     });
+  // };
+
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      username: data.get('Username'),
-      password: data.get('password'),
-    });
-
-
-    const url = "http://localhost:8000/login/admin/auth/login";  // The URL of your API endpoint
-    // const username = "your_username";  // The username for sign-in
-    // const password = "your_password";  // The password for sign-in
-
-    // Prepare the request data
-    data.append("username", username);
-    data.append("password", password);
-
+    console.log("eifybvc kufyvgul")
+    // Create a new FormData object
+    const formData = new FormData();
+  
+    // Append the input values to the formData object
+    formData.append('username', input.username);
+    formData.append('password', input.password);
+  
     // Make the POST request to the API
-    fetch(url, {
-      method: "POST",
-      body: data
+    fetch('http://localhost:80/api/admin/auth/login', {
+      method: 'POST',
+      body: formData,
     })
       .then(response => {
         if (response.ok) {
@@ -71,12 +125,19 @@ const SignIn = () => {
         console.log(`Access Token: ${access_token}`);
         console.log(`Token Type: ${token_type}`);
         console.log(`Role: ${role}`);
+        localStorage.setItem("loggedin", true)
+        nav("/profile")
       })
       .catch(error => {
         // Handle the error response
         console.error(error);
       });
   };
+  
+
+
+
+
 
   const LoginSubmit = (e) => {
     e.preventDefault()
@@ -86,6 +147,7 @@ const SignIn = () => {
     }
   }
 
+  // console
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
@@ -106,22 +168,47 @@ const SignIn = () => {
             Sign in
           </Typography>
           <hr />
-          <Box component="form" onSubmit={LoginSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            {/* <TextField
               margin="normal"
               required
               fullWidth
               id="Username"
               label="Username"
-              name="Username"
-              autoComplete="Username"
-              autoFocus
+              name="username"
+              value={input.username}
+              onChange={(e) => setInput({ ...input, [e.target.name]: e.target.value })}
             />
             <TextField
               margin="normal"
               required
               fullWidth
               name="password"
+              value={input.password}
+              onChange={(e) => setInput({ ...input, [e.target.name]: e.target.value })}
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            /> */}
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="Username"
+              label="Username"
+              name="username"
+              value={input.username}
+              onChange={(e) => setInput({ ...input, [e.target.name]: e.target.value })}
+            />
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              value={input.password}
+              onChange={(e) => setInput({ ...input, [e.target.name]: e.target.value })}
               label="Password"
               type="password"
               id="password"
@@ -136,8 +223,10 @@ const SignIn = () => {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              onClick = {handleSubmit}
             >
               Sign In
+
             </Button>
           </Box>
         </Box>
@@ -145,6 +234,59 @@ const SignIn = () => {
       </Container>
     </ThemeProvider>
   );
-}
 
+
+
+//   return (
+//     <div className="container">
+//         <div className="card mt-4 mx-5">
+//             <div className="card-body">
+//                 <h5 className="card-title my-3">Log In</h5>
+//                 <form onSubmit={handleSubmit}>
+//                     <div className="form-group my-3">
+//                         <label htmlFor="exampleInputEmail1">Username</label>
+//                         <input
+//                             type="text"
+//                             className="form-control"
+//                             id="exampleInputEmail1"
+//                             aria-describedby="emailHelp"
+//                             placeholder="Username"
+//                             required
+//                             name="username" 
+//                             value={input.username} 
+//                             onChange = {(e)=>setInput({...input,[e.target.name]: e.target.value})}
+//                         />
+//                         <small id="emailHelp" className="form-text text-muted">
+//                             We'll never share your details with anyone else.
+//                         </small>
+//                     </div>
+//                     <div className="form-group ">
+//                         <label htmlFor="exampleInputPassword1">Password</label>
+//                         <input
+//                             type="password"
+//                             className="form-control"
+//                             id="exampleInputPassword1"
+//                             placeholder="Password"
+//                             required
+//                             name="password" 
+//                             value={input.password} 
+//                             onChange = {(e)=>setInput({...input,[e.target.name]: e.target.value})}
+//                         />
+//                     </div>
+//                     <div className="my-3 text-center">
+//                         <button type="submit" className="btn btn-primary mx-1">
+//                         Login
+//                         </button>
+//                         {/* <button type="submit" className="btn btn-primary mx-1">
+//                         Not yet registered?
+//                         </button> */}
+//                     </div>
+                    
+//                 </form>
+//             </div>
+//         </div>
+//     </div>
+// );
+// }
+}
 export default SignIn
