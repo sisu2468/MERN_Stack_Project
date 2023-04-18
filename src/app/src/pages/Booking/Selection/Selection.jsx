d8cea0a92b8e78727b05cbda5b7de51a80b2a029
tanalpha-aditya <raghuvanshiadityapiloy@gmail.com>
2023-04-18 10:49:50 +0530

    Updated Selection Page

--- src/app/src/pages/Booking/Selection/Selection.jsx
+++ src/app/src/pages/Booking/Selection/Selection.jsx
@@ -162,123 +162,125 @@ import { useState } from "react";
 // import './LocationDropdowns.css'; // Import custom CSS file for styling
 // import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS for additional styling
 
-const LocationDropdowns = ({ locations }) => {
-    // States for selected values
-    const [selectedCountry, setSelectedCountry] = useState("");
-    const [selectedState, setSelectedState] = useState("");
-    const [selectedCity, setSelectedCity] = useState("");
-    //   const [selectedFloor, setSelectedFloor] = useState("");
-    const [selectedLocation, setSelectedLocation] = useState("");
+const LocationDropdowns = ({ locationsarray }) => {
+  // States for selected values
+  const [locations, setLocations] = useState(locationsarray.locations || []);
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  //   const [selectedFloor, setSelectedFloor] = useState("");
+  const [selectedLocation, setSelectedLocation] = useState("");
 
-    // Filter states, cities, locations, and floors based on selected country, state, and city
-    const filteredStates = Array.from(
-        new Set(
-            locations
-                .filter((location) => location.country === selectedCountry)
-                .map((location) => location.state)
-        )
-    );
-    const filteredCities = Array.from(
-        new Set(
-            locations
-                .filter(
-                    (location) =>
-                        location.country === selectedCountry &&
-                        location.state === selectedState
-                )
-                .map((location) => location.city)
-        )
-    );
+  useEffect(() => {
+    setLocations(locationsarray.locations);
+  }, [locationsarray]);
 
-    const filteredLocations = Array.from(
-        new Set(
-            locations
-                .filter(
-                    (location) =>
-                        location.country === selectedCountry &&
-                        location.state === selectedState &&
-                        location.city === selectedCity
-                )
-                .map((location) => location.name)
-        )
-    );
-    //   const filteredFloors = Array.from(
-    //     new Set(
-    //       locations
-    //         .filter(
-    //           (location) =>
-    //             location.country === selectedCountry &&
-    //             location.state === selectedState &&
-    //             location.city === selectedCity &&
-    //             location.name === selectedLocation
-    //         )
-    //         .map((location) => location.floor)
-    //     )
-    //   );
+  //   const filteredFloors = Array.from(
+  //     new Set(
+  //       locations
+  //         .filter(
+  //           (location) =>
+  //             location.country === selectedCountry &&
+  //             location.state === selectedState &&
+  //             location.city === selectedCity &&
+  //             location.name === selectedLocation
+  //         )
+  //         .map((location) => location.floor)
+  //     )
+  //   );
 
-    return (
+  return (
+    <>
+      {locations && locations.length != 0 ? (
         <div className="location-dropdowns-container">
-            {/* Country dropdown */}
-            <br />
-            <select
-                className="form-control"
-                value={selectedCountry}
-                onChange={(e) => setSelectedCountry(e.target.value)}
-            >
-                <option value="">Select Country</option>
-                {Array.from(new Set(locations.map((location) => location.country))).map(
-                    (country) => (
-                        <option key={country} value={country}>
-                            {country}
-                        </option>
-                    )
-                )}
-            </select>
-            <br />
-            {/* State dropdown */}
-            <select
-                className="form-control"
-                value={selectedState}
-                onChange={(e) => setSelectedState(e.target.value)}
-            >
-                <option value="">Select State</option>
-                {filteredStates.map((state) => (
-                    <option key={state} value={state}>
-                        {state}
-                    </option>
-                ))}
-            </select>
-            <br />
-            {/* City dropdown */}
-            <select
-                className="form-control"
-                value={selectedCity}
-                onChange={(e) => setSelectedCity(e.target.value)}
-            >
-                <option value="">Select City</option>
-                {filteredCities.map((city) => (
-                    <option key={city} value={city}>
-                        {city}
-                    </option>
-                ))}
-            </select>
-            <br />
-            {/* Location dropdown */}
-            <select
-                className="form-control"
-                value={selectedLocation}
-                onChange={(e) => setSelectedLocation(e.target.value)}
-            >
-                <option value="">Select Location</option>
-                {filteredLocations.map((location) => (
-                    <option key={location} value={location}>
-                        {location}
-                    </option>
-                ))}
-            </select>
+          {/* Country dropdown */}
+          <br />
+          <select
+            className="form-control"
+            value={selectedCountry}
+            onChange={(e) => setSelectedCountry(e.target.value)}
+          >
+            <option value="">Select Country</option>
+            {Array.from(
+              new Set(locations.map((location) => location.country))
+            ).map((country) => (
+              <option key={country} value={country}>
+                {country}
+              </option>
+            ))}
+          </select>
+          <br />
+          {/* State dropdown */}
+          <select
+            className="form-control"
+            value={selectedState}
+            onChange={(e) => setSelectedState(e.target.value)}
+          >
+            <option value="">Select State</option>
+            {Array.from(
+              new Set(
+                locations
+                  .filter((location) => location.country === selectedCountry)
+                  .map((location) => location.state)
+              )
+            ).map((state) => (
+              <option key={state} value={state}>
+                {state}
+              </option>
+            ))}
+          </select>
+          <br />
+          {/* City dropdown */}
+          <select
+            className="form-control"
+            value={selectedCity}
+            onChange={(e) => setSelectedCity(e.target.value)}
+          >
+            <option value="">Select City</option>
+            {Array.from(
+              new Set(
+                locations
+                  .filter(
+                    (location) =>
+                      location.country === selectedCountry &&
+                      location.state === selectedState
+                  )
+                  .map((location) => location.city)
+              )
+            ).map((city) => (
+              <option key={city} value={city}>
+                {city}
+              </option>
+            ))}
+          </select>
+          <br />
+          {/* Location dropdown */}
+          <select
+            className="form-control"
+            value={selectedLocation}
+            onChange={(e) => setSelectedLocation(e.target.value)}
+          >
+            <option value="">Select Location</option>
+            {Array.from(
+              new Set(
+                locations
+                  .filter(
+                    (location) =>
+                      location.country === selectedCountry &&
+                      location.state === selectedState &&
+                      location.city === selectedCity
+                  )
+                  .map((location) => location.name)
+              )
+            ).map((location) => (
+              <option key={location} value={location}>
+                {location}
+              </option>
+            ))}
+          </select>
 
-            {/* Floor dropdown */}
-            {/* <select
+          {/* Floor dropdown */}
+          {/* <select
                     className="form-control"
                     value={selectedFloor}
                     onChange={(e) => setSelectedFloor(e.target.value)}
@@ -294,30 +296,39 @@ const LocationDropdowns = ({ locations }) => {
                 </select> 
             */}
         </div>
-    );
+      ) : null}
+    </>
+  );
 };
 
 const Selection = () => {
-    const [locations, setlocations] = useState([]);
-    React.useEffect(() => {
-        const config = {}
-        axios.get('http://localhost:80/api/maps/locations', config, { port: 80 })
-            .then((response) => {
-                setlocations(response.data);
-                // console.log(locations);
-            });
-    }, []);
+  const [locations, setlocations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  React.useEffect(() => {
+    const config = {};
+    axios
+      .get("http://localhost:80/api/maps/locations", config, { port: 80 })
+      .then((response) => {
+        setlocations(response.data);
+        setLoading(false);
+        // console.log(locations);
+      });
+  }, []);
 
-    return (
-        <>
-            <Page full header={"Book your slot"} loading={false} empty={false}>
-                <hr />
-                <LocationDropdowns locations={locations.locations} />
-                <br />
-                <center><button type="button" class="btn btn-success">Book</button></center>
-            </Page>
-        </>
-    );
+  return (
+    <>
+      <Page full header={"Book your slot"} loading={loading} empty={false}>
+        <hr />
+        <LocationDropdowns locationsarray={locations} />
+        <br />
+        <center>
+          <button type="button" className="btn btn-success">
+            Book
+          </button>
+        </center>
+      </Page>
+    </>
+  );
 };
 
 export default Selection;
